0a145cd68fad32eeb19fe317d57d5e431f01a75c
Tiffany Yuen <tiffyuen@users.noreply.github.com>
2017-08-22 03:06:01 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,325 +1,67 @@
-"use strict";
-var APP_ID = "amzn1.ask.skill.2fbb06b7-6727-4231-9c85-8753d52f36fa";  // TODO replace with your app ID (OPTIONAL).
+'use strict';
+var Alexa = require('alexa-sdk');
 
-var ANSWER_COUNT = 4; // The number of possible answers per trivia question.
-var GAME_LENGTH = 5;  // The number of questions per trivia game.
-var QUOTE_STATES = {
-    APP: "_APPMODE", // Asking trivia questions.
-    START: "_STARTMODE", // Entry point, start the game.
-    HELP: "_HELPMODE" // The user is asking for help.
-};
-var quotes = require("./quotes");
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
 
-/**
- * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
- * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
- */
-var languageString = {
-    "en": {
-        "translation": {
-            "QUOTES" : quotes["QUOTES_EN_US"],
-            "APP_NAME" : "Morning Alexa", // Be sure to change this for your skill.
-            "HELP_MESSAGE": "This app aims to provide motivation for the start of your day!",
-            "REPEAT_QUOTE_MESSAGE": "To repeat the quote, say, repeat. ",
-            "ASK_MESSAGE_START": "Would you like to listen to the quote of the day?",
-            "HELP_REPROMPT": "This app aims to provide motivation for the start of your day!",
-            "STOP_MESSAGE": "Would you like another quote?",
-            "CANCEL_MESSAGE": "Ok, have a great morning. Goodbye!",
-            "NO_MESSAGE": "Ok, have a great morning. Goodbye!",
-            "HELP_UNHANDLED": "Say yes for another quote, or no to exit the app.",
-            "START_UNHANDLED": "Would you like some motivation for the start of the day?",
-            "NEW_GAME_MESSAGE": "Welcome to %s. ",
-            "WELCOME_MESSAGE": "Morning! No matter how bad things are, you can at least be happy that you work up this morning -- D.L. Hughley.",
-            "TRIVIA_UNHANDLED": "Would you like another motivational quote for the start of the day?"
-        }
-    },
-    "en-US": {
-        "translation": {
-            "QUESTIONS" : quotes["QUOTES_EN_US"],
-            "GAME_NAME" : "Morning Alexa" // Be sure to change this for your skill.
-        }
-    }
-};
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
+//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+var APP_ID = "amzn1.ask.skill.2fbb06b7-6727-4231-9c85-8753d52f36fa";
 
-//"ANSWER_CORRECT_MESSAGE": "correct. ",
-            //"ANSWER_WRONG_MESSAGE": "wrong. ",
-            //"CORRECT_ANSWER_MESSAGE": "The correct answer is %s: %s. ",
-            //"ANSWER_IS_MESSAGE": "That answer is ",
-            //"TELL_QUESTION_MESSAGE": "Question %s. %s ",
-            //"GAME_OVER_MESSAGE": "You got %s out of %s questions correct. Thank you for playing!",
-            //"SCORE_IS_MESSAGE": "Your score is %s. "
+var SKILL_NAME = "Morning Alexa";
+var GET_FACT_MESSAGE = "Here's your quote: ";
+var HELP_MESSAGE = "You can say tell me a quote, or, you can say exit... What can I help you with?";
+var HELP_REPROMPT = "What can I help you with?";
+var STOP_MESSAGE = "Have a great day! Goodbye!";
 
-var Alexa = require("alexa-sdk");
-var APP_ID = "amzn1.ask.skill.2fbb06b7-6727-4231-9c85-8753d52f36fa";  // TODO replace with your app ID (OPTIONAL).
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
+//=========================================================================================================================================
+var data = [
+    "No matter how bad things are, you can at least be happy that you work up this morning -- D.L. Hughley",
+    "Smile in the mirror. Do that every morning and you'll start to see a big difference in your life -- Yoko Ono",
+    "There was never a night or a problem that could defect sunrise or hope -- Bernard Williams",
+    "When you arise in the morning, thinkg of what a precious privilege it is to be alive - to breathe, to think, to enjoy, to love -- Marcus Aurelius",
+    "Every day I feel is a blessing from God. And I consider it a new beginning. Yeah, everything is beautiful -- Prince",
+    "I wake up every morning literally with a smile on my face, grateful for another day I never thought I'd see -- Dick Cheney",
+    "I have always been delighted at the prospect of a new day, a fresh try, one more start, with perhaps a bit of magic waiting somewhere behind the morning -- J.B. Priestley",
+    "I'm always thinking about creating. My future starts when I wake up every morning... Every day I find something creative to do with my life -- Miles Davis",
+    "Be pleasant until ten o'clock in the morning and the rest of the day will take care of itself -- Elbert Hubbard",
+    "My favorite things in life don't cost any money. It's really clear that the most previous resource we all have is time -- Steve Jobs",
+    "Things don't have to change the world to be important -- Steve Jobs"
+];
 
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.  
+//=========================================================================================================================================
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageString;
-    alexa.registerHandlers(newSessionHandlers, startStateHandlers, triviaStateHandlers, helpStateHandlers);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var newSessionHandlers = {
-    "LaunchRequest": function () {
-        this.handler.state = QUOTE_STATES.START;
-        this.emitWithState("StartApp", true);
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewQuoteIntent');
     },
-    "AMAZON.StartOverIntent": function() {
-        this.handler.state = QUOTE_STATES.START;
-        this.emitWithState("StartApp", true);
+    'GetNewQuoteIntent': function () {
+        var factArr = data;
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = GET_FACT_MESSAGE + randomFact;
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
     },
-    "AMAZON.HelpIntent": function() {
-        this.handler.state = QUOTE_STATES.HELP;
-        this.emitWithState("helpTheUser", true);
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
     },
-    "Unhandled": function () {
-        var speechOutput = this.t("START_UNHANDLED"); // Would you like some motivation for the start of the day?
-        this.emit(":ask", speechOutput, speechOutput);
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
     }
 };
-
-var startStateHandlers = Alexa.CreateStateHandler(QUOTE_STATES.START, {
-    "StartApp": function (newDay) {
-        var speechOutput = newDay ? this.t("NEW_DAY_MESSAGE", this.t("APP_NAME")) + this.t("WELCOME_MESSAGE") : "";
-        // Select GAME_LENGTH questions for the game
-        var translatedQuestions = this.t("QUOTES");
-        var gameQuestions = populateGameQuestions(translatedQuestions);
-        // Generate a random index for the correct answer, from 0 to 3
-        //var correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
-        // Select and shuffle the answers for each question
-        //var roundAnswers = populateRoundAnswers(gameQuestions, 0, correctAnswerIndex, translatedQuestions);
-        var currentQuestionIndex = 0;
-        var spokenQuestion = Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0];
-        var repromptText = this.t("TELL_QUESTION_MESSAGE", "1", spokenQuestion);
-
-        /*for (var i = 0; i < ANSWER_COUNT; i++) {
-            repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". ";
-        }*/
-
-        speechOutput += repromptText;
-        
-        // Trivia answers options
-        /*Object.assign(this.attributes, {
-            "speechOutput": repromptText,
-            "repromptText": repromptText,
-            "currentQuesti onIndex": currentQuestionIndex,
-            "correctAnswerIndex": correctAnswerIndex + 1,
-            "questions": gameQuestions,
-            "score": 0,
-            "correctAnswerText": translatedQuestions[gameQuestions[currentQuestionIndex]][Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0]][0]
-        });*/
-
-        // Set the current state to trivia mode. The skill will now use handlers defined in triviaStateHandlers
-        this.handler.state = QUOTE_STATES.APP;
-        this.emit(":askWithCard", speechOutput, repromptText, this.t("APP_NAME"), repromptText);
-    }
-});
-
-var triviaStateHandlers = Alexa.CreateStateHandler(QUOTE_STATES.APP, {
-    /*"AnswerIntent": function () {
-        handleUserGuess.call(this, false);
-    },
-    "DontKnowIntent": function () {
-        handleUserGuess.call(this, true);
-    },*/
-    "AMAZON.StartOverIntent": function () {
-        this.handler.state = QUOTE_STATES.START;
-        this.emitWithState("StartGame", false);
-    },
-    "AMAZON.RepeatIntent": function () {
-        this.emit(":ask", this.attributes["speechOutput"], this.attributes["repromptText"]);
-    },
-    "AMAZON.HelpIntent": function () {
-        this.handler.state = QUOTE_STATES.HELP;
-        this.emitWithState("helpTheUser", false);
-    },
-    "AMAZON.StopIntent": function () {
-        this.handler.state = QUOTE_STATES.HELP;
-        var speechOutput = this.t("STOP_MESSAGE");
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "AMAZON.CancelIntent": function () {
-        this.emit(":tell", this.t("CANCEL_MESSAGE"));
-    },
-    "Unhandled": function () {
-        var speechOutput = this.t("TRIVIA_UNHANDLED", ANSWER_COUNT.toString());
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "SessionEndedRequest": function () {
-        console.log("Session ended in Morning Alexa state: " + this.event.request.reason);
-    }
-});
-
-var helpStateHandlers = Alexa.CreateStateHandler(QUOTE_STATES.HELP, {
-    "helpTheUser": function (newGame) {
-        var askMessage = newGame ? this.t("ASK_MESSAGE_START") : this.t("REPEAT_QUESTION_MESSAGE") + this.t("STOP_MESSAGE");
-        var speechOutput = this.t("HELP_MESSAGE", GAME_LENGTH) + askMessage;
-        var repromptText = this.t("HELP_REPROMPT") + askMessage;
-        this.emit(":ask", speechOutput, repromptText);
-    },
-    "AMAZON.StartOverIntent": function () {
-        this.handler.state = QUOTE_STATES.START;
-        this.emitWithState("StartGame", false);
-    },
-    "AMAZON.RepeatIntent": function () {
-        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
-        this.emitWithState("helpTheUser", newGame);
-    },
-    "AMAZON.HelpIntent": function() {
-        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
-        this.emitWithState("helpTheUser", newGame);
-    },
-    "AMAZON.YesIntent": function() {
-        if (this.attributes["speechOutput"] && this.attributes["repromptText"]) {
-            this.handler.state = QUOTE_STATES.APP;
-            this.emitWithState("AMAZON.RepeatIntent");
-        } else {
-            this.handler.state = QUOTE_STATES.START;
-            this.emitWithState("StartGame", false);
-        }
-    },
-    "AMAZON.NoIntent": function() {
-        var speechOutput = this.t("NO_MESSAGE");
-        this.emit(":tell", speechOutput);
-    },
-    "AMAZON.StopIntent": function () {
-        var speechOutput = this.t("STOP_MESSAGE");
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "AMAZON.CancelIntent": function () {
-        this.emit(":tell", this.t("CANCEL_MESSAGE"));
-    },
-    "Unhandled": function () {
-        var speechOutput = this.t("HELP_UNHANDLED");
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "SessionEndedRequest": function () {
-        console.log("Session ended in help state: " + this.event.request.reason);
-    }
-});
-
-/*function handleUserGuess(userGaveUp) {
-    //var answerSlotValid = isAnswerSlotValid(this.event.request.intent);
-    var speechOutput = "";
-    var speechOutputAnalysis = "";
-    var gameQuestions = this.attributes.questions;
-    var correctAnswerIndex = parseInt(this.attributes.correctAnswerIndex);
-    var currentScore = parseInt(this.attributes.score);
-    var currentQuestionIndex = parseInt(this.attributes.currentQuestionIndex);
-    var correctAnswerText = this.attributes.correctAnswerText;
-    var translatedQuestions = this.t("QUESTIONS");
-
-    if (answerSlotValid && parseInt(this.event.request.intent.slots.Answer.value) == this.attributes["correctAnswerIndex"]) {
-        currentScore++;
-        speechOutputAnalysis = this.t("ANSWER_CORRECT_MESSAGE");
-    } else {
-        if (!userGaveUp) {
-            speechOutputAnalysis = this.t("ANSWER_WRONG_MESSAGE");
-        }
-
-        speechOutputAnalysis += this.t("CORRECT_ANSWER_MESSAGE", correctAnswerIndex, correctAnswerText);
-    }
-
-    // Check if we can exit the game session after GAME_LENGTH questions (zero-indexed)
-    if (this.attributes["currentQuestionIndex"] == GAME_LENGTH - 1) {
-        speechOutput = userGaveUp ? "" : this.t("ANSWER_IS_MESSAGE");
-        speechOutput += speechOutputAnalysis + this.t("GAME_OVER_MESSAGE", currentScore.toString(), GAME_LENGTH.toString());
-
-        this.emit(":tell", speechOutput)
-    } else {
-        currentQuestionIndex += 1;
-        //correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
-        var spokenQuestion = Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0];
-        //var roundAnswers = populateRoundAnswers.call(this, gameQuestions, currentQuestionIndex, correctAnswerIndex, translatedQuestions);
-        var questionIndexForSpeech = currentQuestionIndex + 1;
-        var repromptText = this.t("TELL_QUESTION_MESSAGE", questionIndexForSpeech.toString(), spokenQuestion);
-
-        for (var i = 0; i < ANSWER_COUNT; i++) {
-            repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". "
-        }
-
-        speechOutput += userGaveUp ? "" : this.t("ANSWER_IS_MESSAGE");
-        speechOutput += speechOutputAnalysis + this.t("SCORE_IS_MESSAGE", currentScore.toString()) + repromptText;
-
-        Object.assign(this.attributes, {
-            "speechOutput": repromptText,
-            "repromptText": repromptText,
-            "currentQuestionIndex": currentQuestionIndex,
-            //"correctAnswerIndex": correctAnswerIndex + 1,
-            "questions": gameQuestions,
-            //"score": currentScore,
-            "correctAnswerText": translatedQuestions[gameQuestions[currentQuestionIndex]][Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0]][0]
-        });
-
-        this.emit(":askWithCard", speechOutput, repromptText, this.t("GAME_NAME"), repromptText);
-    }
-}
-*/
-function populateGameQuestions(translatedQuestions) {
-    var gameQuestions = [];
-    var indexList = [];
-    var index = translatedQuestions.length;
-
-    if (GAME_LENGTH > index){
-        throw new Error("Invalid Game Length.");
-    }
-
-    for (var i = 0; i < translatedQuestions.length; i++){
-        indexList.push(i);
-    }
-
-    // Pick GAME_LENGTH random questions from the list to ask the user, make sure there are no repeats.
-    for (var j = 0; j < GAME_LENGTH; j++){
-        var rand = Math.floor(Math.random() * index);
-        index -= 1;
-
-        var temp = indexList[index];
-        indexList[index] = indexList[rand];
-        indexList[rand] = temp;
-        gameQuestions.push(indexList[index]);
-    }
-
-    return gameQuestions;
-}
-
-/**
- * Get the answers for a given question, and place the correct answer at the spot marked by the
- * correctAnswerTargetLocation variable. Note that you can have as many answers as you want but
- * only ANSWER_COUNT will be selected.
- * */
-/*function populateRoundAnswers(gameQuestionIndexes, correctAnswerIndex, correctAnswerTargetLocation, translatedQuestions) {
-    //var answers = [];
-    //var answersCopy = translatedQuestions[gameQuestionIndexes[correctAnswerIndex]][Object.keys(translatedQuestions[gameQuestionIndexes[correctAnswerIndex]])[0]].slice();
-    //var index = answersCopy.length;
-
-    //if (index < ANSWER_COUNT) {
-    //    throw new Error("Not enough answers for question.");
-    //}
-
-    // Shuffle the answers, excluding the first element which is the correct answer.
-    //for (var j = 1; j < answersCopy.length; j++){
-        var rand = Math.floor(Math.random() * (index - 1)) + 1;
-        index -= 1;
-
-        var temp = answersCopy[index];
-        answersCopy[index] = answersCopy[rand];
-        answersCopy[rand] = temp;
-    }
-
-    // Swap the correct answer into the target location
-    for (var i = 0; i < ANSWER_COUNT; i++) {
-        answers[i] = answersCopy[i];
-    }
-    temp = answers[0];
-    answers[0] = answers[correctAnswerTargetLocation];
-    answers[correctAnswerTargetLocation] = temp;
-    return answers;
-}*/
-
-/*function isAnswerSlotValid(intent) {
-    var answerSlotFilled = intent && intent.slots && intent.slots.Answer && intent.slots.Answer.value;
-    var answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value));
-    return answerSlotIsInt && parseInt(intent.slots.Answer.value) < (ANSWER_COUNT + 1) && parseInt(intent.slots.Answer.value) > 0;
-}*/
